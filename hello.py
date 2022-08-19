d8870e786ce1eb1f94edfe5d639facfe9a047584
catingblack <www.catingblack@gmail.com>
2022-08-19 12:33:08 +0700

    3

--- hello.py
+++ hello.py
@@ -10,8 +10,7 @@ def app(environ, start_response):
     args = q_str.split('&')
     
     body = ''
-    body = body + "Hello"
-    
+  
     for str in args:
         body = body + str + '\n'
        
