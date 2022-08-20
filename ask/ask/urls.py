a4785742e92fea5ae690d30091324f14eca9e4f6
catingblack <www.catingblack@gmail.com>
2022-08-20 12:26:18 +0700

    django_4

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,17 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, url
 
 urlpatterns = [
-    path('/', include('qa.urls')),
+    """path('/', include('qa.urls')),
     path('/login/', include('qa.urls')),
     path('/signup/', include('qa.urls')),
     path('/ask/', include('qa.urls')),
     path('/popular/', include('qa.urls')),
     path('/new/', include('qa.urls')),
     path('/question/<int:id>/', include('qa.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), """ 
+    
+    
+    url(r'^$', include('qa.urls')),
+    url(r'^login/', include('qa.urls')),
+    url(r'^signup/', include('qa.urls')),
+    url(r'^ask/', include('qa.urls')),
+    url(r'^popular/', include('qa.urls'))
+    url(r'^new/', include('qa.urls')),
+    url(r'^question/(\d+)/', include('qa.urls')),
+    url(r'^admin/', admin.site.urls),
+    
 ]
 
 
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    #path('', views.test, name='test'),
+    url(r'^', views.test, name='test'),
 ]
