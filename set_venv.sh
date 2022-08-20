7d503548b48c6174a97ec12daade68faf9dcbc52
catingblack <www.catingblack@gmail.com>
2022-08-20 20:17:49 +0700

    dog5

--- set_venv.sh
+++ set_venv.sh
@@ -1,6 +1,5 @@
-sudo apt update
-sudo apt install python3.5
 
 virtualenv -p "/usr/bin/python3.5" env 2 > /dev/null
 source env/bin/activate
-pip install gunicorn django==2.0
+pip install gunicorn django==2.
+
