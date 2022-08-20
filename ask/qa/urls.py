de4614a80c836792aa0ffc2a1a87343b72ed9b0f
catingblack <www.catingblack@gmail.com>
2022-08-20 19:39:03 +0700

    0

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test'),
+    #path('', views.test, name='test'),
+    url(r'^', views.test, name='test'),
 ]
