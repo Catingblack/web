7caa3ea2e670f9ad2afe39430d640f10eba11523
catingblack <www.catingblack@gmail.com>
2022-08-22 12:28:36 +0700

    add db finis!

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,19 +3,19 @@ from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):
     def new(self):
-        return self.order_by('-added_at')[0:10]
+        return self.order_by('-added_at')
     
     def popular(self):
-        return self.order_by('rating')[0:10]
+        return self.order_by('-rating')
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField(blank=True)
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
-    rating = models.IntegerField(blank=True)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
-    likes = models.ManyToManyField(User, related_name="likes_users", on_delete=models.DO_NOTHING)
-    objects = QuestionManager
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True)
+    likes = models.ManyToManyField(User, related_name="likes_users")
+    objects = QuestionManager()
     
 class Answer(models.Model):
     text = models.TextField()
