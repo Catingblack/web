2681dbf3e46c67051f19550b59fd00809ac953f3
catingblack <www.catingblack@gmail.com>
2022-08-22 09:26:10 +0700

    add db v8

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -10,16 +10,16 @@ class QuestionManager(models.Manager):
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    text = models.TextField(blank=True)
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, default="admin")
     likes = models.ManyToManyField(User, related_name="likes_users")
     objects = QuestionManager
     
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
