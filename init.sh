f6459512866672c932a125d2d8c85b55d2e53610
catingblack <www.catingblack@gmail.com>
2022-08-22 07:34:02 +0700

    add db v2

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 if [[ -f /etc/nginx/sites-enabled/default ]]; then
 	sudo rm /etc/nginx/sites-enabled/default
 	sudo chown -R www-data:www-data /home/box/web
-	sudo chmod -R 755 /home/box/web/
+	sudo chmod -R 777 /home/box/web/
 	sudo chown -R www-data:www-data /var/log/nginx
 	sudo chmod -R 777 /var/log/nginx
 fi
@@ -15,11 +15,3 @@ sudo chown -R www-data:www-data /etc/nginx
 sudo chmod -R 755 /etc/nginx/
 sudo /etc/init.d/nginx restart
 
-sudo /etc/init.d/mysql start
-
-mysql -u root -e "create database ask_db"
-mysql -u root -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
-mysql -u root -e  "GRANT ALL PRIVILEGES ON ask_db.* TO 'box'@'localhost' WITH GRANT OPTION;"
-
-./manage.py makemigrations
-./manage.py migrate
