2b94845695104d4d7c8fffe5cccdc58b5e4595f8
catingblack <www.catingblack@gmail.com>
2022-08-22 12:34:30 +0700

    transport db.sh and update.sh in init.sh

--- ask/db.sh
+++
@@ -1,9 +0,0 @@
-
-sudo /etc/init.d/mysql start
-
-mysql -u root -e "create database ask_db"
-mysql -u root -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
-mysql -u root -e  "GRANT ALL PRIVILEGES ON ask_db.* TO 'box'@'localhost' WITH GRANT OPTION;"
-
-python3 manage.py makemigrations qa
-python3 manage.py migrate
--- init.sh
+++ init.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#update
+
+sudo apt update
+sudo apt install python3.5 -y
+sudo apt install python3.5-dev -y
+sudo apt install mysql-server-5.6
+
+
+sudo rm /usr/bin/python3                         
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install gunicorn
+sudo pip3 install django==2.1
+sudo pip3 install mysqlclient
+
+
+#config and start nginx and mysql
 
 if [[ -f /etc/nginx/sites-enabled/default ]]; then
 	sudo rm /etc/nginx/sites-enabled/default
@@ -14,4 +30,18 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo chown -R www-data:www-data /etc/nginx
 sudo chmod -R 755 /etc/nginx/
 sudo /etc/init.d/nginx restart
+
+sudo /etc/init.d/mysql start
+
+
+#set mysql and migrations
+
+mysql -u root -e "create database ask_db"
+mysql -u root -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
+mysql -u root -e  "GRANT ALL PRIVILEGES ON ask_db.* TO 'box'@'localhost' WITH GRANT OPTION;"
+
+python3 /home/box/web/ask/manage.py makemigrations qa
+python3 /home/box/web/ask/manage.py migrate
+
+
 
