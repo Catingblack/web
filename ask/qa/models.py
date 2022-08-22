a21005fed9af0ff08d3b044d44e2393a35f1d1da
catingblack <www.catingblack@gmail.com>
2022-08-22 09:29:04 +0700

    fix models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -11,7 +11,7 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField(blank=True)
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(blank=True)
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING, default="admin")
     likes = models.ManyToManyField(User, related_name="likes_users")
@@ -19,7 +19,7 @@ class Question(models.Model):
     
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
