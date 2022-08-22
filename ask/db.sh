2598fc4ee777063135f1ec405b8319c3959441c7
catingblack <www.catingblack@gmail.com>
2022-08-22 07:57:37 +0700

    add db v3

--- ask/db.sh
+++ ask/db.sh
@@ -5,5 +5,5 @@ mysql -u root -e "create database ask_db"
 mysql -u root -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
 mysql -u root -e  "GRANT ALL PRIVILEGES ON ask_db.* TO 'box'@'localhost' WITH GRANT OPTION;"
 
-./manage.py makemigrations
-./manage.py migrate
+python3 manage.py makemigrations qa
+python3 manage.py migrate
--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 sudo apt update
-sudo apt install python3.5
+sudo apt install python3.5 -y
+sudo apt install python3.5-dev -y
 sudo apt install mysql-server-5.6
 
 sudo rm /usr/bin/python3                         
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 sudo pip3 install gunicorn
 sudo pip3 install django==2.1
+sudo pip3 install mysqlclient
