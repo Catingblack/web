b0323fe57b8dae6c2d649eb97a3ae5c19c89a37a
catingblack <www.catingblack@gmail.com>
2022-08-25 23:01:20 +0700

    v0

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,35 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_GET
+from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator
+from qa.models import *
 
 
 def paginate(request, qs):
      try: 
-        page = int(requests.GET.get('page', 1))
+        page = int(request.GET.get('page', 1))
      except ValueError:
         raise Http404
-    paginator = Paginator(qs, limit=10)
-    paginator.baseurl = '/?page='
+    paginator = Paginator(qs, 10)
     try: 
         page = paginator.page(page)
     except EmptyPage:
         page = paginator.page(paginator.num_pages)
-    return page
+    return page, paginator
         
         
-
-@require_GET
-def home(request, *args, **kwargs):
-    return HttpResponse('OK')
+        
 
 @require_GET
 def new(request):
     qs = Question.objects.new()
-    paginator = Paginator(qs, limit=10)
-    page = paginate(request, qs)
     
-    return render(request, 'new.html',  {
+    page, paginator = paginate(request, qs)
+    paginator.baseurl = reverse('new') + '?page='
+    
+    return render(request, 'qa/new.html',  {
         
         'questions': page.object_list,
         'paginator': paginator,
@@ -40,10 +39,11 @@ def new(request):
 @require_GET
 def popular(request):
     qs = Question.objects.popular()
-    paginator = Paginator(qs, limit=10) 
-    page = paginate(request, qs)
     
-    return render(request, 'popular.html',  {
+    page, paginator = paginate(request, qs)
+    paginator.baseurl = reverse('popular') + '?page='
+ 
+    return render(request, 'qa/popular.html',  {
         'questions': page.object_list,
         'paginator': paginator,
         'page': page,
@@ -54,7 +54,7 @@ def popular(request):
 def question_details(request, id):
     question = get_object_or_404(Question, id=id)
 
-    return render(request, 'question_details.html',  {
+    return render(request, 'qa/question_details.html',  {
         'question': question,
         'id': id,    
     })
