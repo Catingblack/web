5b6c4ab1ab1b598a061bda7207aac12e67668cdb
catingblack <www.catingblack@gmail.com>
2022-08-26 05:11:09 +0700

    v4

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_GET
 from django.urls import reverse
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 from qa.models import *
 
 
