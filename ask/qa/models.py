39a1a7c1e9ff0f4fee3cd1164e994c7385a6860b
catingblack <www.catingblack@gmail.com>
2022-08-26 05:17:00 +0700

    fix models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):
     def new(self):
-        return self.order_by('-added_at')[:10]
+        return self.order_by('-added_at')
     
     def popular(self):
-        return self.order_by('-rating')[:10]
+        return self.order_by('-rating')
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
