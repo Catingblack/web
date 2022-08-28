993af191f7d68401a31a7fda9db2ad3b8f71896c
catingblack <www.catingblack@gmail.com>
2022-08-28 19:31:28 +0700

    fix pk in Question models reverse

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
     
     def popular(self):
         return self.order_by('-rating')
+
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -19,8 +21,9 @@ class Question(models.Model):
     objects = QuestionManager()
 
     def get_url(self):
-        return reverse('question', kwargs={'id': self.id})
-    
+        return reverse('question', kwargs={'id': self.pk})
+
+
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
